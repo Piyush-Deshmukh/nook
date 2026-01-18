a556cd99c33beaebba618657cadfc3b9c387debb
Piyush-Deshmukh <piyushd2112@gmail.com>
2026-01-18 13:41:16 +0530

    update captions and add new image

--- src/data/polaroid.ts
+++ src/data/polaroid.ts
@@ -3,11 +3,13 @@ import nvimSetup from "@/public/polaroids/nvim-setup.png";
 import waterfall from "@/public/polaroids/original/waterfall.jpg";
 import ground from "@/public/polaroids/original/ground.jpg";
 import principles from "@/public/polaroids/original/principles.jpg";
+import farm from "@/public/polaroids/original/farm.jpg";
 
 export const Polaroids = [
-  { src: cssArt, alt: "CSS Art" },
-  { src: waterfall, alt: "Waterfall" },
-  { src: nvimSetup, alt: "Neovim" },
-  { src: ground, alt: "Ground" },
-  { src: principles, alt: "Principles" },
+  { src: cssArt, alt: "css art" },
+  { src: waterfall, alt: "waterfall" },
+  { src: nvimSetup, alt: "neovim" },
+  { src: ground, alt: "mornings" },
+  { src: principles, alt: "principles" },
+  { src: farm, alt: "stillness" },
 ];
